f3166038c84356209342fb036311d821764ee1c6
Deniz Ozdemir <denizozd@c4b6c2.42berlin.de>
2023-11-27 19:12:00 +0100

    beginning

--- get_next_line.c
+++ get_next_line.c
@@ -6,49 +6,28 @@
 /*   By: denizozd <denizozd@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/27 15:27:50 by denizozd          #+#    #+#             */
-/*   Updated: 2023/11/27 17:26:58 by denizozd         ###   ########.fr       */
+/*   Updated: 2023/11/27 19:11:35 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-#include <stdio.h> //to be deleted
-
-void	print_newline_helper(char *buffer)
+static int	read_buffer(int fd, char **storage, char *buffer)
 {
-	while (*buffer && *buffer != '\0')
-	{
-		if (*buffer == '\n')
-		{
-			*buffer = '\\';
-		}
-		printf("%c", *buffer);
-		buffer++;
-	}
-}
-
-static char *read_from_file(int fd)
-{
-	int	bytes_read;
-	char	*cup_buffer;
-	static int	count = 1;
-
-	printf("ft_calloc#[%d]---", count++);
-	cup_buffer = ft_calloc (BUFFER_SIZE + 1, sizeof(char));
-	if (!cup_buffer)
-		return (NULL);
-	bytes_read = read(fd, cup_buffer, BUFFER_SIZE);
-	print_newline_helper(cup_buffer);
-
-	if (bytes_read <= 0)
-		return (free (cup_buffer), NULL);
-	return (cup_buffer);
-}
+	
 
 char	*get_next_line(int fd)
 {
-	char *basin_buffer;
+	static char	*storage;
+	char	*line;
+	char	*buffer;
+	int	bytes;	
 
-	basin_buffer = read_from_file(fd);
-	return (basin_buffer);
+	storage = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
+	bytes = 1;
+	while (strchr(storage, '\n') == NULL && bytes > 0)
+		bytes = read_buffer(fd, &storage, buffer);
 }
